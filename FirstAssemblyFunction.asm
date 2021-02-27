986749bd626b14d5542985ea1275a575cdf34460
Willie Ling <willie.ling@outlook.com>
2021-02-27 17:49:47 -0500

    Two new functions return the passed in function Add two values and return their sum

--- FirstAssemblyFunction.asm
+++ FirstAssemblyFunction.asm
@@ -3,4 +3,15 @@ FirstAssemblyFunction proc
 	mov rax, 64
 	ret
 FirstAssemblyFunction endp
+
+Print proc
+	mov rax, rcx
+	ret
+Print endp
+
+AddTwoInts proc
+	add rcx, rdx
+	mov rax, rcx
+	ret
+AddTwoInts endp
 end
--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 
 extern "C" int FirstAssemblyFunction();
+extern "C" int Print(int value);
+extern "C" int AddTwoInts(int value1, int value2);
 
 int main()
 {
-	std::cout << "The result is: " << FirstAssemblyFunction() << std::endl;
+	std::cout << "FirstAssemblyFunction: " << FirstAssemblyFunction() << std::endl;
+	std::cout << "Print: " << Print(456) << std::endl;
+	std::cout << "AddTwoInts: " << AddTwoInts(10, 20) << std::endl;
+
 	return 0;
 }
